b424a53430500c6f2eba6ad84d601b732e15a0a7
Nobuaki Mochizuki <hagaeru3sei@gmail.com>
2017-10-31 16:56:41 +0900

    Add download form

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,24 @@
 import Vue from 'vue/dist/vue.js'
 import axios from 'axios'
+import downloadjs from 'downloadjs'
 import Vuetable from 'vuetable-2/src/components/Vuetable.vue'
 Vue.use(Vuetable);
 
 const msg = "Convert native2ascii for java apps";
 const languageEndpoint = 'http://localhost:8800/lang';
 const endpoint = 'http://localhost:8800/api';
+
+
+function getLanguageCodes() {
+  let languages = [];
+  axios.get(languageEndpoint).then(res => {
+    console.log(res);
+    languages = res.data.languages
+  });
+  console.log(languages);
+  return languages;
+}
+
 
 // attach message
 new Vue({
@@ -105,4 +118,75 @@ const viewApp = new Vue({
   }
 });
 
+// download
+const downloadApp = new Vue({
+  el: "#downloadForm",
+  data: {
+    selectedLanguage: 'en',
+    languages: []
+  },
+  mounted: function() {
+    this.$nextTick(function () {
+      this.get()
+    })
+  },
+  methods: {
+    get: function() {
+      axios.get(languageEndpoint).then(res => {
+        console.log("GET /lang response", res);
+        this.languages = res.data.languages
+      });
+    },
+    download: function () {
+      axios.get(endpoint + "/dl/" + this.selectedLanguage).then(response => {
+        console.log("response:", response);
+        downloadjs(response.data, 'message_' + this.selectedLanguage + '.properties')
+      })
+    }
+  }
+});
 
+// upload property file
+const uploadApp = new Vue({
+  el : '#uploadForm',
+  data: {
+    selectedLanguage: 'en',
+    languages: [],
+    file: null
+  },
+  mounted: function() {
+    this.$nextTick(function () {
+      this.get()
+    })
+  },
+  methods: {
+    get: function() {
+      axios.get(languageEndpoint).then(res => {
+        console.log("GET /lang response", res);
+        this.languages = res.data.languages
+      });
+    },
+    selectedFile: function(event) {
+      console.log("selectedFile");
+      event.preventDefault();
+      let files = event.target.files;
+      this.file = files[0]
+    },
+    upload: function () {
+      console.log("upload method called");
+      let formData = new FormData();
+      formData.append('upload_file', this.file);
+      let config = {
+        headers : {
+          'content-type' : 'multipart/form-data'
+        }
+      };
+      let url = endpoint + '/upload/' + this.selectedLanguage;
+      axios.post(url, formData, config).then(res => {
+        console.log("response", res)
+      }).catch(e =>
+        console.log("error", e)
+      );
+    }
+  }
+});
